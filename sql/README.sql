5a450ca2089420a8998f3646d98b5bf3591208df
Dom <157792207+lunarjuun@users.noreply.github.com>
2026-01-28 13:41:30 +0100

    Fix SQL table definitions and constraints

--- sql/README.sql
+++ sql/README.sql
@@ -1,29 +1,27 @@
 CREATE TABLE `Kunde` (
-  `Kunde_ID` PRIMARY KEY INT,
-  `Navn` VARCHAR,
-  `Passord` VARCHAR,
-  `Epost` VARCHAR UNIQUE NOT NULL
+  `Kunde_ID` INT PRIMARY KEY,
+  `Navn` VARCHAR(50),
+  `Passord` VARCHAR(50),
+  `Epost` VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE `Bil` (
-  `Bil_ID` PRIMARY KEY INT,
-  `Bil_Merke` VARCHAR,
-  `Bil_Modell` VARCHAR,
+  `Bil_ID` INT PRIMARY KEY,
+  `Bil_Merke` VARCHAR(50),
+  `Bil_Modell` VARCHAR(50),
   `Årsmodell` INT,
   `Dagspris` DECIMAL NOT NULL,
-  `Registreringsnummer` VARCHAR UNIQUE NOT NULL
+  `Registreringsnummer` VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE `Utleieavtale` (
-  `Avtale_ID` PRIMARY KEY INT,
-  `Kunde_ID` FOREIGN KEY INT NOT NULL,
-  `Bil_ID` FOREIGN KEY INT NOT NULL,
+  `Avtale_ID` INT PRIMARY KEY,
+  `Kunde_ID` INT NOT NULL,
+  `Bil_ID` INT NOT NULL,
+  FOREIGN KEY (Kunde_ID) REFERENCES Kunde(Kunde_ID),
+  FOREIGN KEY (Bil_ID) REFERENCES Bil(Bil_ID)
   `StartDato` DATE,
   `SluttDato` DATE,
   `Totalpris` DECIMAL NOT NULL,
-  FOREIGN KEY (`Kunde_ID`)
-      REFERENCES `Kunde`(`Kunde_ID`),
-  FOREIGN KEY (`Bil_ID`)
-      REFERENCES `Bil`(`Bil_ID`)
 );
 
